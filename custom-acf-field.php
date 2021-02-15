708ca3d3ba6471902c5bc4b87321665407822b84
anton152 <anton@ictin.us>
2021-02-14 21:03:20 -0500

    initial commit

---
+++ custom-acf-field.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: Custom ACF Field
+ * Plugin URI: https://avoy.me
+ * Description: Custom ACF Field
+ * Author: avoy18
+ * Author URI: https://avoy.me
+ * Version: 1.0.0
+ * License: GPL2+
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Block Initializer.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'inc/init.php';
